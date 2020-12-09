d15a38e520b2653f14732824c8f1cdbe9a095846
David Kuda <david.kuda@pm.me>
2020-12-09 22:42:22 +0100

    refactor: Split code in two parts: Use once, use again

--- pnd_check_attendance.js
+++ pnd_check_attendance.js
@@ -1,8 +1,26 @@
+///////
+// 
+// Insert the following block once per session
+// 
+// 
+
 let names = [];
 
 const is_present = (name) => {
 	return document.body.parentElement.innerHTML.includes(name)
 };
+
+const print_group_members = (group) => {
+	for (member of group) {
+		console.log(member)
+	}
+};
+
+// 
+// 
+// Insert the following block every time you want to check presence:
+// 
+// 
 
 let presentees = [];
 let absentees = [];
@@ -15,14 +33,12 @@ for (name of names) {
 	}
 };
 
-const print_group_members = (group) => {
-	for (member of group) {
-		console.log(member)
-	}
-};
-
 console.log('%c These People Are Present:', 'color: dodgerblue')
 print_group_members(presentees)
 
 console.log('%c These People Are Absent:', 'color: coral')
 print_group_members(absentees)
+
+// 
+// 
+///////
